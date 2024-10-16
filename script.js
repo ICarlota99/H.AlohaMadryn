c5d0892630bf650bafd75301f57e4f070db99696
ICarlota99 <k431999@gmail.com>
2024-10-15 21:01:40 -0400

    modified script to add footer

--- script.js
+++ script.js
@@ -15,17 +15,6 @@ fetch('navbar.html')
     });
 
 // Fetch footer
-fetch('footer.html')
-    // Convert the response in plain text
-    .then(response => response.text())
-
-    .then(data => {
-        // Find the container div
-        const footerContainer = document.getElementById('footer-container');
-
-        // Insert the footer HTML into the container
-        footerContainer.innerHTML = data;
-    })
-    .catch(error => {
-        console.error('Error fetching footer:', error);
-    });
+$(function(){
+    $("#footer").load("footer.html");
+})
